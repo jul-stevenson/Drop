5453c70c2d3ee16a69df94982dd022772c2104a6
Julianna Stevenson <steve146@purdue.edu>
2015-11-21 17:33:18 -0600

    emergency contact UI added

--- Drop/ViewController.swift
+++ Drop/ViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 import CoreMotion
 import Foundation
+import AddressBook
 
 class ViewController: UIViewController {
     
@@ -26,14 +27,22 @@ class ViewController: UIViewController {
             (data, error) in
             dispatch_async(dispatch_get_main_queue()) {
                 self.accel = sqrt(self.accel*pow(data!.acceleration.x, 2) + self.accel*pow(data!.acceleration.y, 2) + self.accel*pow(data!.acceleration.z, 2))
+
                 
                 if(self.accel > 9.0 && self.accel < 20.0){
+                    var noButtonPressed = true
+                    
                     var refreshAlert = UIAlertController(title: "Alert", message: "Are you ok? You seem to have fallen down!", preferredStyle: UIAlertControllerStyle.Alert)
                     
                     refreshAlert.addAction(UIAlertAction(title: "Yes", style: .Default, handler: { (action: UIAlertAction!) in
+                        noButtonPressed = false
+                        print("Yes " + String(noButtonPressed))
                     }))
                     
                     refreshAlert.addAction(UIAlertAction(title: "No", style: .Default, handler: { (action: UIAlertAction!) in
+                        
+                        noButtonPressed = false
+                        print("No " + String(noButtonPressed))
                         
                         if let url = NSURL(string: "tel://\(self.phone)") {
                             UIApplication.sharedApplication().openURL(url)
@@ -42,13 +51,17 @@ class ViewController: UIViewController {
                     }))
                     
                     self.presentViewController(refreshAlert, animated: true, completion: nil)
-                    
+
                     let delay = 15.0 * Double(NSEC_PER_SEC)
                     var time = dispatch_time(DISPATCH_TIME_NOW, Int64(delay))
                     dispatch_after(time, dispatch_get_main_queue(), {
-                        if let url = NSURL(string: "tel://\(self.phone)") {
-                            UIApplication.sharedApplication().openURL(url)
-                        }
+                        print("Wait " + String(noButtonPressed))
+                        //if(refreshAlert.){
+                            print("In If Wait " + String(noButtonPressed))
+                            if let url = NSURL(string: "tel://\(self.phone)") {
+                                UIApplication.sharedApplication().openURL(url)
+                            }
+                        //}
                     })
                 }
             }
