980e30ec07d60a4db59dfa4a94231d9c9d91776e
Julianna Stevenson <steve146@purdue.edu>
2015-11-21 14:16:04 -0600

    initial commit

--- Drop/ViewController.swift
+++ Drop/ViewController.swift
@@ -7,12 +7,42 @@
 //
 
 import UIKit
+import CoreMotion
+import Foundation
 
 class ViewController: UIViewController {
+    
+    let manager = CMMotionManager()
+    var fallCount = 0
+    var accel = Double()
+    var largestVal = 0.0
+    var hasFallen = false
+    
+    @IBOutlet weak var accelZ: UILabel!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        accel = 1
+        
+        self.manager.startAccelerometerUpdatesToQueue(NSOperationQueue()) {
+            (data, error) in
+            dispatch_async(dispatch_get_main_queue()) {
+                self.accel = sqrt(self.accel*pow(data!.acceleration.x, 2) + self.accel*pow(data!.acceleration.y, 2) + self.accel*pow(data!.acceleration.z, 2))
+                
+                if(self.accel > 9.0 && self.accel < 20.0 && !self.hasFallen){
+                    self.fallCount++
+                    self.hasFallen = true
+                }
+                else{
+                    self.hasFallen = false
+                }
+                
+                self.accelZ.text = String(self.fallCount)
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -22,4 +52,3 @@ class ViewController: UIViewController {
 
 
 }
-
